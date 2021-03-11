132ed521497088ec6de0e5b6413f5081b9e2a05e
unknown <suhravshan@gmail.com>
2021-03-11 12:59:47 +0600

    im angry

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import AllClubs from './Components/AllClubs/AllClubs';
 import ClubDetails from './Components/ClubDetails/ClubDetails';
